77c532e3aa64589044fc393c96efab754854c245
Marian Cichy <Mn.cichy@gmail.com>
2024-02-23 09:37:49 +0100

    nvim: migrate to astronvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,53 @@
+if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
+
+for _, source in ipairs {
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
+
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify(
+      ("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
+      vim.log.levels.ERROR
+    )
+  end
+end
+
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
+
+vim.opt.wrap = true
+
+--require('telekasten').setup({
+--  home = vim.fn.expand("~/zettelkasten"), -- Put the name of your notes directory here
+--  new_note_filename = "uuid",
+--  show_tags_theme = "get_cursor",
+--})
+
+require("zk").setup({
+  -- can be "telescope", "fzf", "fzf_lua" or "select" (`vim.ui.select`)
+  -- it's recommended to use "telescope", "fzf" or "fzf_lua"
+  picker = "telescope",
+
+  lsp = {
+    -- `config` is passed to `vim.lsp.start_client(config)`
+    config = {
+      cmd = { "zk", "lsp" },
+      name = "zk",
+      -- on_attach = ...
+      -- etc, see `:h vim.lsp.start_client()`
+    },
+
+    -- automatically attach buffers in a zk notebook that match the given filetypes
+    auto_attach = {
+      enabled = true,
+      filetypes = { "markdown" },
+    },
+  },
+})
